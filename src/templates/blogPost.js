d311c2a9b4ceb7c37c539f4ee40d611c5a7138a8
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-20 17:17:24 +1000

    get html from markdown

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,9 +1,30 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
+
 const Template = props => {
-  console.log("template::", props);
-  return <div>blog posts here!</div>;
+  //   console.log("template::", props);
+  const {
+    html,
+    frontmatter: { title }
+  } = props.data.markdownRemark;
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div className="blogpost" dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  );
 };
 
 export default Template;
