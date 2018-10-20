47e5e6b1745d8ca42c56a79bdd96a2f00c2e0f9c
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-20 14:50:16 +1000

    update header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { StaticQuery, graphql } from "gatsby";
 
+const TitleAndDescription = data => {
+  const { title, defaultDescription } = data.data.site.siteMetadata;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontFamily: "avenir"
+      }}
+    >
+      <h2 style={{ marginBottom: 0 }}>{title}</h2>
+      <p style={{ marginTop: 0, opacity: 0.5 }}>{defaultDescription}</p>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+              defaultDescription
+            }
+          }
+        }
+      `}
+      render={data => <TitleAndDescription data={data} />}
+    />
+  );
+};
 const Layout = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Layout;
