3c0fec1dee43677d2670ac965d444bde5cf17bf3
Sam Zhao <zhao.shengjun@gmail.com>
2018-10-24 23:43:37 +1000

    add font

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,13 +6,29 @@ export const Bio = () => (
     <div className="Page-alignment Bio-container">
       <img src={Avatar} alt="avatar" className="Bio-avatar" />
       <div className="Bio-content">
-        <p className="Category--changelog">
+        <p className="Category--changelog Bio-content-detail">
           Hi! I’m <b className="Category--engineering">Sam Zhao</b>, a developer
           based in Brisbane QLD.
         </p>
-        <p className="Category--changelog">
+        <p className="Category--changelog Bio-content-detail">
           I have a particular interest in web/mobile development. Here are the
           tips and tricks I share with you and hope you find it useful.
+        </p>
+        <p className="Category--changelog Bio-content-detail">
+          Check me on{" "}
+          <a href="" className="Link--primary Bio-link ">
+            GitHub
+          </a>
+          ,{" "}
+          <a href="" className="Link--primary Bio-link ">
+            {" "}
+            Twitter{" "}
+          </a>{" "}
+          or{" "}
+          <a href="" className="Link--primary Bio-link ">
+            LinkedIn{" "}
+          </a>
+          .
         </p>
       </div>
     </div>
